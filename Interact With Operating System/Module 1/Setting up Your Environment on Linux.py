6011e121472bdefa870f4fe1ce6424189b0da560
Biuuu2309 <minhvu7a1@gmail.com>
2024-10-11 14:36:23 +0700

    Update Setting up Your Environment on Linux.py

--- Interact With Operating System/Module 1/Setting up Your Environment on Linux.py
+++ Interact With Operating System/Module 1/Setting up Your Environment on Linux.py
@@ -1,3 +1,10 @@
-from PIL import Image
-image = Image.open("./houses.png")
-print(image.size)
+# from PIL import Image
+# image = Image.open("./houses.png")
+# print(image.size)
+
+import pandas
+visitor = [1234, 9764, 3456, 2345, 7643]
+err = [23, 12, 34, 55, 77]
+df = pandas.DataFrame({"Visitor": visitor, "Error": err}, index = ["Mon", "Tues", "Wed", "Thu", "Fri"])
+print(df)
+print(df["Error"].mean())
