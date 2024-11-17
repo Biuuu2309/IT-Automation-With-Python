c17a19cbce155c24e188394e89365d0f8f0eacca
Biuuu2309 <minhvu7a1@gmail.com>
2024-11-17 10:34:03 +0700

    aaa

---
+++ Interact With Operating System/Module 4/Filtering log files with regular expressions.py
@@ -0,0 +1,72 @@
+#!/bin/env/python3
+
+import sys
+
+logfile = sys.argv[1]
+with open(logfile) as f:
+  for line in f:
+    print(line.strip())
+
+
+
+#!/bin/env/python3
+
+import sys
+
+logfile = sys.argv[1]
+with open(logfile) as f:
+  for line in f:
+    if "CRON" not in line:
+      continue
+    print(line.strip())
+
+
+
+import re
+pattern = r"USER \((\w+)\)$"
+line = "Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)"
+result = re.search(pattern, line)
+print(result[1])
+
+
+
+#!/bin/env/python3
+
+import re
+import sys
+
+logfile = sys.argv[1]
+with open(logfile) as f:
+  for line in f:
+    if "CRON" not in line:
+      continue
+    pattern = r"USER \((.+)\)$"
+    result = re.search(pattern, line)
+    print(result[1])
+
+
+chmod +x check_cron.py 
+./check_cron.py syslog 
+
+
+
+#!/bin/env/python3
+
+import re
+import sys
+
+logfile = sys.argv[1]
+usernames = {}
+with open(logfile) as f:
+  for line in f:
+    if "CRON" not in line:
+      continue
+    pattern = r"USER \((\w+)\)$"
+    result = re.search(pattern, line)
+
+    if result is None:
+      continue
+    name = result[1]
+    usernames[name] = usernames.get(name, 0) + 1
+
+print(usernames)
